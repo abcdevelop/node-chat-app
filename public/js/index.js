e7537d6475e622bcb4d92c6cc86421cc67ed3bfe
abcdevelop <bruno.panaget@yahoo.fr>
2017-10-24 17:04:52 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,18 +3,18 @@ var socket = io();
 socket.on('connect',function(){
     console.log('Connected to server');
 
-
-    socket.emit('createMessage',{
-        from:'bruno@example.com',
-        text:'Hey. This is Bruno'
-    });
-});
-
-socket.on('disconnect',function(){
-    console.log('Disconnected from server');
+    // socket.emit('createMessage',{
+    //     from:'bruno@example.com',
+    //     text:'Hey. This is Bruno'
+    // });
 });
 
 
 socket.on('newMessage',function(message){
     console.log('New message',message);
 });
+
+socket.on('disconnect',function(){
+    console.log('Disconnected from server');
+});
+
