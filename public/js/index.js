cbb0478b211b42fab3db003fab5abc54515c3577
abcdevelop <bruno.panaget@yahoo.fr>
2017-10-25 11:07:08 +0200

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -11,9 +11,30 @@ socket.on('connect',function(){
 
 socket.on('newMessage',function(message){
     console.log('New message',message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    jQuery('#messages').append(li);
 });
 
 socket.on('disconnect',function(){
     console.log('Disconnected from server');
 });
 
+// socket.emit('createMessage',{
+//     from:'bruno@example.com',
+//     text:'Hey. This is Bruno'
+// }, function(data){
+//     console.log('Got it.',data);
+// });
+
+jQuery('#message-form').on('submit',function(e){
+   e.preventDefault();
+
+    socket.emit('createMessage',{
+       from: 'User',
+       text: jQuery('[name=message]').val()
+    }, function(){
+
+    });
+});
