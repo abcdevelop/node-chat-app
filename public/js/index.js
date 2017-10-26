f30c6b4bec12755a6ee9705fe0fa9fcd2993eaec
abcdevelop <bruno.panaget@yahoo.fr>
2017-10-26 10:58:13 +0200

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,23 @@
 var socket = io();
 
+function scrollToBotom(){
+    // Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    // Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+}
+
+
 socket.on('connect',function(){
     console.log('Connected to server');
 
@@ -19,6 +37,7 @@ socket.on('newMessage',function(message){
     });
 
     jQuery('#messages').append(html);
+    scrollToBotom();
 
     // //console.log('New message',message);
     //var text = `${message.from} ${formatedTime}: ${message.text}`;
@@ -38,6 +57,7 @@ socket.on('newLocationMessage', function(message){
     });
 
     jQuery('#messages').append(html);
+    scrollToBotom();
 
     // var li = jQuery('<li></li>');
     // var a = jQuery('<a target="_blank">My current location</a>');
